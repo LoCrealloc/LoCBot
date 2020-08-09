850082977f0c0fefb098b4b686194552436baca2
LoC <locpython@gmx.de>
2020-08-09 21:46:41 +0200

    Bugfixes bei den Sprachkanälen

--- main.py
+++ main.py
@@ -66,13 +66,13 @@ async def on_member_join(guild: discord.Guild, user: discord.User):
 @bot.event
 async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     try:
-        if after.channel is not None and after.channel.id == 742105036422643822:
+        if after.channel is not None and after.channel.id == 742106374116474961:
             category: discord.CategoryChannel = discord.utils.get(member.guild.categories, id=554265879463067659)
             await category.create_voice_channel(name="Kanal von " + member.nick)
             channel = discord.utils.get(member.guild.voice_channels, name="Kanal von " + member.nick)
             await member.move_to(channel)
 
-        if after.channel is not None and after.channel.id == 742105077418033212:
+        if after.channel is not None and after.channel.id == 742106421230960652:
             category: discord.CategoryChannel = discord.utils.get(member.guild.categories, id=554265879463067659)
             await category.create_voice_channel(name="Musikkanal von " + member.nick)
             channel: discord.VoiceChannel = discord.utils.get(member.guild.voice_channels,
@@ -82,8 +82,8 @@ async def on_voice_state_update(member: discord.Member, before: discord.VoiceSta
             await member.move_to(channel)
 
         try:
-            if after.channel is None and not before.channel.id == 742105036422643822 or after.channel \
-                    is None and not before.channel.id == 742105077418033212:
+            if after.channel is None and not before.channel.id == 742106374116474961 or after.channel \
+                    is None and not before.channel.id == 742106421230960652:
 
                 if not before.channel.members:
                     await before.channel.delete()
